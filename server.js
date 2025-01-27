ff8ed19ee0c514acabeed65ef2778afe999b6ca1
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-01-26 16:18:01 -0800

    creating basic express server

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
+//set the view folder where ejs is stored
+app.set('views', path.join(__dirname, 'views'));
+//serve static files from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//route for homepage
+app.get('/', (req, res) => {
+    res.render('homePage.ejs')
+})
+
+//route for schools page
+app.get('/schools', (req, res) => {
+    res.render('schools')
+})
+
+app.listen(3000);
