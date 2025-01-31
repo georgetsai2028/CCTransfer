a6cf87416841e8fd81b501c6c3ccd1341b9504ac
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-01-30 19:51:40 -0800

    edited the mysql users table

--- CCTransfer.session.sql
+++ CCTransfer.session.sql
@@ -1,12 +1,7 @@
-CREATE TABLE Users(
+CREATE TABLE `users`(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    --unique id for each user
     email VARCHAR(255) NOT NULL UNIQUE,
-    --unique email thats not null
     username VARCHAR(50) NOT NULL UNIQUE,
-    -- unique username thats not null
     password_hash VARCHAR(255) NOT NULL,
-    --hashed password use bcrypt first
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    --user timestamp creation
-)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
