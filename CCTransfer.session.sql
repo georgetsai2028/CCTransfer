9c1d9c506a3b28bc594f0d20b6dd7612de01d95f
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-01-30 20:59:15 -0800

    testing and running some queries

--- CCTransfer.session.sql
+++ CCTransfer.session.sql
@@ -1,7 +1,19 @@
-CREATE TABLE `users`(
+CREATE TABLE users(
     id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     username VARCHAR(50) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+INSERT INTO users (email, username, password_hash)
+VALUES (
+        'hello@world.com',
+        'pukaaa',
+        'puka123'
+    );
+SELECT id,
+    email,
+    username
+FROM users
+ORDER BY id DESC
+LIMIT 3;
