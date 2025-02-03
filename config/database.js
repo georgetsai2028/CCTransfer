95a35b44a0572a4a207b837f35a4edf5c82e5b61
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-02 16:37:07 -0800

    made db and added env vars, make ignore file next

---
+++ config/database.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: process.env.MYSQL_WAITFORCONNECTIONS,
+    connectionLimit: process.env.MYSQL_CONNECTIONLIMIT,  
+    queueLimit: process.env.MYSQL_QUEUELIMIT        
+});
+
+module.exports = pool.promise();
+
+async function fetchUsers() {
+    try {
+        const [rows, fields] = await pool.promise().query('SELECT * FROM users'); 
+        console.log(rows);
+    } catch (error) {
+        console.error('Error executing query', error);
+    }
+}
+
+fetchUsers();
