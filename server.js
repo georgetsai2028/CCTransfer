2b69404ad01ca9f913549a49eb19858ec9e345e0
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-03 22:27:50 -0800

    creating route for auth

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+
+dotenv.config(); //loads env vars
+
 
 app.set('view engine', 'ejs');
 //set the view folder where ejs is stored
@@ -9,6 +14,9 @@ app.set('views', path.join(__dirname, 'views'));
 //serve static files from public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//body parser middleware to parse form data or JSON body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //for parsing form
 
 //route for homepage
 app.get('/homePage', (req, res) => {
@@ -39,5 +47,7 @@ app.get('/register', (req, res) =>{
     res.render('register/register');
 });
 
+//moute auth route on /auth path
+app.use('/auth', authRoutes);
 
 app.listen(3000);
