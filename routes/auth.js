9d9fe233626715cfc47f6f346e96a0e4fb9c88b0
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-03 22:18:18 -0800

    created auth.js and added route

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const pool = require('../config/database'); //imports database connection
+
+const router = express.Router();
+
+//Registration Route
+router.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        //hashes password before storing
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //inserts users from form into database
+        const sql = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+        const [result] = await pool.query(sql, [username, email, hashedPassword]);
+
+        res.status(201).json({ message: 'User registered successfully', userId: result.insertId});
+
+    } catch (error) {
+        console.error('Error registering user: ', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+module.exports = router;
